d04f77a7d57fbe2398cb56c041ab06af960ab1c2
GzINOD <gzinod31@rambler.ru>
2022-11-17 21:01:09 +0300

    HomeWork done

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Write a number");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+
+if (num1 / 100 >=1)
+{
+     while (num1/100>=10)
+     {
+        num1=num1/10;
+     }
+    if (num1 / 100 >=1 && num1 / 100 < 10)
+    {
+        num1 = num1%10;
+        Console.WriteLine(num1);
+    }
+}
+else 
+{
+    Console.WriteLine("Wrong");
+}
